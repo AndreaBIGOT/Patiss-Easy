9b8781b9edd9bac3045a108a83609000b7354f63
Andréa <70588437+AndreaBIGOT@users.noreply.github.com>
2021-05-26 11:38:39 +0400

    BDD

--- private/patisseasy (1).sql
+++ private/patisseasy (1).sql
@@ -24,11 +24,12 @@ CREATE TABLE `categorie` (
   `idCateg` int(11) NOT NULL AUTO_INCREMENT,
   `categorie` varchar(50) NOT NULL,
   PRIMARY KEY (`idCateg`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 INSERT INTO `categorie` (`idCateg`, `categorie`) VALUES
 (1,	'Cakes'),
-(2,	'Macarons');
+(2,	'Macarons'),
+(3,	'Easy');
 
 DROP TABLE IF EXISTS `doctrine_migration_versions`;
 CREATE TABLE `doctrine_migration_versions` (
@@ -92,11 +93,13 @@ CREATE TABLE `preparation` (
   PRIMARY KEY (`idPreparation`),
   KEY `idRecette` (`idRecette`),
   CONSTRAINT `preparation_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 INSERT INTO `preparation` (`idPreparation`, `etape`, `idRecette`) VALUES
 (1,	'1/ Faites fondre le chocolat.\r\n2/ Séparez les blancs des jaunes et montez les blancs en neige.\r\n3/ Une fois le chocolat refroidi, mélangez-le avec les jaunes. ',	1),
-(2,	'1/ Mélangez le sucre glace avec la poudre d\'amande. \r\n2/ Fouettez en meringue les blancs d\'oeufs.',	2);
+(2,	'1/ Mélangez le sucre glace avec la poudre d\'amande. \r\n2/ Fouettez en meringue les blancs d\'oeufs.',	2),
+(3,	'Fondre le chocolat. \r\nAjouter la farine. \r\nIncorporez les blancs à la maryse.',	29),
+(4,	'public function __toString()\r\n    {\r\n        return $this->categorie;\r\n    }public function __toString()\r\n    {\r\n        return $this->categorie;\r\n    }public function __toString()\r\n    {\r\n        return $this->categorie;\r\n    }',	30);
 
 DROP TABLE IF EXISTS `recette`;
 CREATE TABLE `recette` (
@@ -106,25 +109,15 @@ CREATE TABLE `recette` (
   `datePublication` datetime NOT NULL,
   `petitePhrase` text,
   PRIMARY KEY (`idRecette`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 
 INSERT INTO `recette` (`idRecette`, `recette`, `descriptionRecette`, `datePublication`, `petitePhrase`) VALUES
-(1,	'Gateau choco',	'Gâteau trop bon',	'2012-12-12 22:20:23',	'Gâteau parfait pour le gouter!'),
+(1,	'Gâteau chocolat banane',	'Ce gâteau est hyper moelleux. \r\n',	'2012-12-12 22:20:23',	'Gâteau parfait pour le gouter!'),
 (2,	'Macarons',	'Recette inratable!!!!!!',	'2023-08-21 20:23:00',	'Recette pour petits et grands!'),
 (3,	'aqzsedrfgh',	'zsqedrftgyhfdsrfgvfcdsedrfgvcdsfv',	'2021-04-27 07:11:17',	'df'),
 (4,	'categ',	'categ',	'2021-04-27 07:31:12',	'categ'),
-(5,	'mlp',	'mlp',	'2021-04-27 07:47:04',	'mlp'),
-(6,	'fghjk',	'gfhjk',	'2021-04-27 07:50:01',	'ghjk'),
-(7,	'categ',	'dd($recette);',	'2021-04-29 09:46:22',	'dd($recette);'),
-(8,	'test categ',	'test categ',	'2021-05-03 04:18:33',	'test categ'),
-(9,	'ing',	'ing',	'2021-05-03 04:27:26',	'inginginginging'),
-(10,	'ustensile',	'ustensile',	'2021-05-03 04:34:12',	'ustensileustensileustensile'),
-(16,	'aaaa',	'aaaa',	'2021-05-03 06:49:17',	'aaaa'),
-(17,	'Andréa',	'Andréa',	'2021-05-03 10:17:07',	'AndréaAndréaAndréaAndréa'),
-(18,	'Andréa',	'Andréa',	'2021-05-03 10:19:42',	'AndréaAndréaAndréaAndréa'),
-(19,	'Andréa',	'Andréa',	'2021-05-03 10:23:04',	'AndréaAndréaAndréaAndréa'),
-(20,	'add ing',	'add ing',	'2021-05-03 10:24:09',	'add ing'),
-(21,	'ajouter ing',	'ajouter ing',	'2021-05-03 10:50:07',	'ajouter ing');
+(29,	'Préparation',	'PréparationPréparationPréparationPréparation',	'2021-05-26 05:06:07',	'PréparationPréparation'),
+(30,	'unité',	'unitéunitéunité',	'2021-05-26 06:55:17',	'unitéunité');
 
 DROP TABLE IF EXISTS `recette_astuce`;
 CREATE TABLE `recette_astuce` (
@@ -132,18 +125,14 @@ CREATE TABLE `recette_astuce` (
   `idAstuce` int(11) NOT NULL,
   PRIMARY KEY (`idRecette`,`idAstuce`),
   KEY `IDX_10E715796A667F1A` (`idAstuce`),
-  CONSTRAINT `recette_astuce_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`),
-  CONSTRAINT `recette_astuce_ibfk_2` FOREIGN KEY (`idAstuce`) REFERENCES `astuce` (`idAstuce`)
+  CONSTRAINT `recette_astuce_ibfk_3` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recette_astuce_ibfk_4` FOREIGN KEY (`idAstuce`) REFERENCES `astuce` (`idAstuce`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `recette_astuce` (`idRecette`, `idAstuce`) VALUES
 (1,	1),
-(16,	1),
-(17,	1),
-(18,	1),
-(19,	1),
-(20,	1),
-(21,	1);
+(29,	1),
+(30,	1);
 
 DROP TABLE IF EXISTS `recette_categ`;
 CREATE TABLE `recette_categ` (
@@ -151,57 +140,51 @@ CREATE TABLE `recette_categ` (
   `idCateg` int(11) NOT NULL,
   PRIMARY KEY (`idRecette`,`idCateg`),
   KEY `IDX_FF7A49E5144A826E` (`idCateg`),
-  CONSTRAINT `recette_categ_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`),
-  CONSTRAINT `recette_categ_ibfk_2` FOREIGN KEY (`idCateg`) REFERENCES `categorie` (`idCateg`)
+  CONSTRAINT `recette_categ_ibfk_3` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recette_categ_ibfk_4` FOREIGN KEY (`idCateg`) REFERENCES `categorie` (`idCateg`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `recette_categ` (`idRecette`, `idCateg`) VALUES
 (1,	1),
-(6,	1),
-(8,	1),
-(9,	1),
-(10,	1),
-(16,	1),
-(17,	1),
-(18,	1),
-(19,	1),
-(20,	1),
-(21,	1),
+(29,	1),
+(30,	1),
 (2,	2),
-(6,	2),
-(8,	2),
-(10,	2),
-(17,	2),
-(18,	2),
-(19,	2),
-(20,	2);
+(1,	3),
+(29,	3);
 
 DROP TABLE IF EXISTS `recette_ingredient`;
 CREATE TABLE `recette_ingredient` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idRecette` int(11) NOT NULL,
   `idIng` int(11) NOT NULL,
-  `quantite` varchar(15) NOT NULL,
+  `quantite` float NOT NULL,
+  `unite` varchar(4) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_17C041A9B1A786E8` (`idIng`),
-  KEY `recette_ingredient_ibfk_1` (`idRecette`),
-  CONSTRAINT `recette_ingredient_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`),
-  CONSTRAINT `recette_ingredient_ibfk_2` FOREIGN KEY (`idIng`) REFERENCES `ingredient` (`idIng`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  KEY `idRecette` (`idRecette`),
+  CONSTRAINT `recette_ingredient_ibfk_3` FOREIGN KEY (`idIng`) REFERENCES `ingredient` (`idIng`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recette_ingredient_ibfk_4` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
-INSERT INTO `recette_ingredient` (`id`, `idRecette`, `idIng`, `quantite`) VALUES
-(1,	1,	2,	'0'),
-(2,	1,	7,	'0'),
-(3,	2,	1,	'0'),
-(4,	2,	6,	'0'),
-(5,	9,	1,	'0'),
-(6,	9,	2,	'0'),
-(7,	10,	1,	'0'),
-(8,	10,	2,	'0'),
-(9,	10,	3,	'0'),
-(10,	10,	4,	'0'),
-(11,	16,	3,	'0'),
-(12,	16,	8,	'0');
+INSERT INTO `recette_ingredient` (`id`, `idRecette`, `idIng`, `quantite`, `unite`) VALUES
+(1,	1,	2,	1150,	'g'),
+(2,	1,	7,	150,	'g'),
+(3,	2,	1,	1.5,	'l'),
+(4,	2,	6,	60,	'g'),
+(30,	29,	7,	200,	'g'),
+(31,	29,	9,	100,	'g'),
+(32,	30,	6,	200,	'g');
+
+DELIMITER ;;
+
+CREATE TRIGGER `before_insert_quantite` BEFORE INSERT ON `recette_ingredient` FOR EACH ROW
+BEGIN
+    IF (NEW.quantite < 0) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT="Veuillez entrer une quantité supérieur à 0.";
+    END IF;
+END;;
+
+DELIMITER ;
 
 DROP TABLE IF EXISTS `recette_theme`;
 CREATE TABLE `recette_theme` (
@@ -209,20 +192,15 @@ CREATE TABLE `recette_theme` (
   `idTheme` int(11) NOT NULL,
   PRIMARY KEY (`idRecette`,`idTheme`),
   KEY `IDX_6B816F9E80B1A415` (`idTheme`),
-  CONSTRAINT `recette_theme_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`),
-  CONSTRAINT `recette_theme_ibfk_2` FOREIGN KEY (`idTheme`) REFERENCES `theme` (`idTheme`)
+  CONSTRAINT `recette_theme_ibfk_2` FOREIGN KEY (`idTheme`) REFERENCES `theme` (`idTheme`),
+  CONSTRAINT `recette_theme_ibfk_3` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `recette_theme` (`idRecette`, `idTheme`) VALUES
 (1,	1),
-(16,	1),
-(17,	1),
-(18,	1),
-(19,	1),
-(20,	1),
-(21,	1),
-(2,	2),
-(16,	2);
+(29,	1),
+(30,	1),
+(2,	2);
 
 DROP TABLE IF EXISTS `recette_ustensile`;
 CREATE TABLE `recette_ustensile` (
@@ -230,21 +208,17 @@ CREATE TABLE `recette_ustensile` (
   `idUstensile` int(11) NOT NULL,
   PRIMARY KEY (`idRecette`,`idUstensile`),
   KEY `IDX_613487D52E08C24D` (`idUstensile`),
-  CONSTRAINT `recette_ustensile_ibfk_1` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`),
-  CONSTRAINT `recette_ustensile_ibfk_2` FOREIGN KEY (`idUstensile`) REFERENCES `ustensile` (`idUstensile`)
+  CONSTRAINT `recette_ustensile_ibfk_3` FOREIGN KEY (`idRecette`) REFERENCES `recette` (`idRecette`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recette_ustensile_ibfk_4` FOREIGN KEY (`idUstensile`) REFERENCES `ustensile` (`idUstensile`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `recette_ustensile` (`idRecette`, `idUstensile`) VALUES
 (2,	1),
-(10,	1),
-(16,	1),
-(17,	1),
-(18,	1),
-(19,	1),
-(20,	1),
-(21,	1),
+(29,	1),
+(30,	1),
 (1,	2),
-(10,	2);
+(29,	2),
+(30,	2);
 
 DROP TABLE IF EXISTS `theme`;
 CREATE TABLE `theme` (
@@ -284,4 +258,4 @@ INSERT INTO `ustensile` (`idUstensile`, `ustensile`) VALUES
 (1,	'Robot'),
 (2,	'Fouet');
 
--- 2021-05-04 05:20:09
+-- 2021-05-26 07:35:37
